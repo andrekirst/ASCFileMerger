4598bcc768f654e61dbf2fa1fb38b04dca530f5b
andrekirst <github@andrekirst.de>
2017-06-17 20:23:49 +0200

    Solution und Projekte hinzugefügt

---
+++ ASCFileMerger/ASCFileMerger.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ASCFileMerger.Test
+{
+    [TestClass]
+    public class T_Merger
+    {
+        [TestMethod]
+        public void Merger_()
+        {
+        }
+    }
+}
